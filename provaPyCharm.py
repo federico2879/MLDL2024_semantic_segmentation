ade0368f51872fe564a6c4e24900e60099c68b5c
fede <folivero2001@gmail.com>
2024-05-03 13:00:33 +0200

    prova primo commit pycharm

---
+++ provaPyCharm.py
@@ -0,0 +1,7 @@
+#prova push da pycharm
+import random
+class ProvaPyCharm:
+    def __init__(self):
+        self.attributo1 = random.randint(1, 100)
+        self.attributo2 = random.choice(['a', 'b', 'c', 'd'])
+        self.attributo3 = random.uniform(0.0, 1.0)
