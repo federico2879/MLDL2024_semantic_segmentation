c8d44f486e52d85eafc65c94a2410cf499a879b3
fabiodanielediena <126665927+fabiodanielediena@users.noreply.github.com>
2024-05-07 12:55:44 +0200

    Update cityscapes.py
    correzione

--- datasets/cityscapes.py
+++ datasets/cityscapes.py
@@ -6,7 +6,7 @@ from torch.utils.data import Dataset
 
 class CityScapes(Dataset):
     def __init__(self, root_dir, split = 'train', transform=None, target_transform=None):
-        super(CityScapes, self)._init_()
+        super(CityScapes, self).__init__()
         self.root_dir = root_dir
         self.image_dir = os.path.join(root_dir, 'images', split)
         self.label_dir = os.path.join(root_dir, 'gtFine', split)
