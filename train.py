ae3b2b9f089ac60d811cc7dad7e1fec5766b5f15
federico2879 <128393678+federico2879@users.noreply.github.com>
2024-05-14 16:20:54 +0200

    Update train.py
    modified train and test functions, correct metric applied

--- train.py
+++ train.py
@@ -1,4 +1,3 @@
-# TODO: Define here your training and validation loops.
 import torch
 import torchvision
 
@@ -9,11 +8,35 @@ def train(model, optimizer, train_loader, loss_fn):
     total = 0
 
     for batch_idx, (inputs, targets) in enumerate(train_loader):
+
+
+        #print(f'batch id: {batch_idx}')
+        #print(f'(inputs, targets): {(inputs.size(), targets.size())}')
+        first_image = inputs[0]
+
+        # Stampiamo le dimensioni della prima immagine nel batch
+        #print("Dimensioni della prima immagine nel batch:", first_image.size())
         inputs, targets = inputs.cuda(), targets.cuda()
 
         # Compute prediction and loss
         outputs =  model(inputs)
-        loss = loss_fn(outputs, targets)
+        '''
+       # print(f'outputs[0]: {outputs[0]}')
+        print(f'outputs[0] type: {outputs[0].type()}')
+        print(f'outputs[0] size: {outputs[0].size()}')
+
+
+        #print(f'targets: {targets}')
+        print(f'targets type: {targets.type()}')
+        print(f'targets size: {targets.size()}')
+        '''
+        #Ridimensioning tensor
+        targets = targets.squeeze(dim=1)
+        #print(f'targets size: {targets.size()}')
+
+        targets = targets.long()
+
+        loss = loss_fn(outputs[0], targets)
 
         # Backpropagation
         optimizer.zero_grad() # reset gradients of parameters
@@ -21,7 +44,7 @@ def train(model, optimizer, train_loader, loss_fn):
         optimizer.step() # update model
 
         running_loss += loss.item()
-        _, predicted = outputs.max(1)
+        _, predicted = outputs[0].max(1)
         total += targets.size(0)
         correct += predicted.eq(targets).sum().item()
 
@@ -33,18 +56,50 @@ def test(model, test_loader, loss_fn):
     model.eval()
     test_loss = 0
     correct = 0
-    total = 0
+    total_images = 0
+    total_iou = 0
     with torch.no_grad():
         for batch_idx, (inputs, targets) in enumerate(test_loader):
             inputs, targets = inputs.cuda(), targets.cuda()
             outputs = model(inputs)
+            #Ridimensioning tensor+
+            '''
+            print(f'outputs: {outputs}')
+            print(f'outputs type: {outputs.type()}')
+            print(f'outputs size: {outputs.size()}')
+
+
+            print(f'outputs[0]: {outputs[0]}')
+
+            print(f'outputs[0] type: {outputs[0].type()}')
+            print(f'outputs[0] size: {outputs[0].size()}')
+
+
+            #pri nt(f'targets: {targets}')
+            print(f'targets type: {targets.type()}')
+            print(f'targets size: {targets.size()}')
+            '''
+            targets = targets.squeeze(dim=1)
+
+            #print(f'targets size: {targets.size()}')
+
+            targets = targets.long()
+            #print(f'targets type: {targets.type()}')
+            #print(f'targets size: {targets.size()}')
             loss = loss_fn(outputs, targets)
 
             test_loss += loss.item()
             _, predicted = outputs.max(1)
-            total += targets.size(0)
-            correct += predicted.eq(targets).sum().item()
+            #print(f'predicted: {predicted}')
+            iou = meanIOU(outputs.size()[1], predicted, targets) #sum of meanIOU over classes di tutte le immagini nel batch
+            #total += targets.size(0)
+            #correct += predicted.eq(targets).sum().item()
+            total_iou += iou.sum().item()  #somma di tytte le singole iou calcolate in precedenza
 
-    test_loss = test_loss / len(test_loader)
-    test_accuracy = 100. * correct / total
-    return test_accuracy
+            #print(f'len di targets (=batch_size?): {len(targets)}')
+            total_images += len(targets)
+
+    result= total_iou/total_images
+    #test_loss = test_loss / len(test_loader)
+    #test_accuracy = 100. * correct / total
+    return result
