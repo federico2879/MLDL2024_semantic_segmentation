18f12c22f66cc7c869a491cde3844b454d8227ee
federico2879 <128393678+federico2879@users.noreply.github.com>
2024-05-21 15:22:56 +0200

    Update gta5.py

--- datasets/gta5.py
+++ datasets/gta5.py
@@ -1,22 +1,42 @@
 from torch.utils.data import Dataset
-
-# TODO: implement here your custom dataset class for GTA5
-
+import os
+import numpy as np
+from PIL import Image
+from torch.utils.data import Dataset
+import torch
 
 class GTA5(Dataset):
-    def __init__(self):
+    def __init__(self, root_dir, transform=None, label_transform=None):
         super(GTA5, self).__init__()
-        # TODO
-
-        pass
+        self.root_dir = root_dir
+        self.image_dir = os.path.join(root_dir, 'images')
+        self.label_dir = os.path.join(root_dir, 'labels')
+        self.transform = transform
+        self.label_transform = label_transform
+        self.images = os.listdir(self.image_dir)
 
     def __getitem__(self, idx):
-        # TODO
+        img_name = self.images[idx]
+        img_path = os.path.join(self.image_dir, img_name)
+        label_name = img_name 
+        #.replace('leftImg8bit', 'gtFine_labelTrainIds')
+        label_path = os.path.join(self.label_dir, label_name)
+        
+        image = Image.open(img_path).convert('RGB')
+        label = Image.open(label_path)
+        
 
-        pass
+        if self.transform is not None:
+            image = self.transform(image)
+
+        if self.label_transform is not None:
+            label = self.label_transform(label)
+
+        return image, label
 
     def __len__(self):
+        return len(self.images)
 
-        # TODO
-
-        pass
+def print_random():
+    print('cjaso')
+    
