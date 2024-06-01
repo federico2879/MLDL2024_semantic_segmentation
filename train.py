aecffca32800fa6f62352d8676b2976eb6602a03
fabiodanielediena <126665927+fabiodanielediena@users.noreply.github.com>
2024-06-01 17:13:27 +0200

    Update train.py
    add parameter

--- train.py
+++ train.py
@@ -1,7 +1,8 @@
 import torch
 import torchvision
 import gc
-from MLDL2024_semantic_segmentation.models.metrics import meanIOU
+import numpy as np
+from MLDL2024_semantic_segmentation.models.IOU import meanIOU
 #from MLDL2024_semantic_segmentation.models.metrics import fast_hist
 #from MLDL2024_semantic_segmentation.models.metrics import per_class_iou
 
@@ -11,11 +12,12 @@ def clear_gpu_memory():
     torch.cuda.empty_cache()
     gc.collect()
 
-def train(model, optimizer, train_loader, loss_fn, clear_memory_every):
+def train(model, optimizer, train_loader, loss_fn, num_classes, clear_memory_every):
     model.train()
     running_loss = 0.0
     correct = 0
     total_iou = 0
+    total_iou_cl = np.zeros([1,num_classes])
     total_images = 0
 
     for batch_idx, (inputs, targets) in enumerate(train_loader):
@@ -42,23 +44,27 @@ def train(model, optimizer, train_loader, loss_fn, clear_memory_every):
 
         running_loss += loss.item()
         _, predicted = outputs[0].max(1)
-        iou = meanIOU(outputs[0].size()[1], predicted, targets) #sum of meanIOU over classes di tutte le immagini nel batch
-        total_iou += iou.sum().item()  #somma di tytte le singole iou calcolate in precedenza
+        iou, iou_cl = meanIOU(num_classes, predicted, targets) #sum of meanIOU over classes di tutte le immagini nel batch
+        total_iou += iou
+        total_iou_cl += iou_cl
         total_images += len(targets)
         
         # Clear GPU memory periodically
         if clear_memory_every!=0 and batch_idx % clear_memory_every == 0:
             clear_gpu_memory()
 
-    result= total_iou/total_images
-    return result
+    miou = total_iou/total_images
+    iou_class = total_iou_cl/total_images
+    return miou, iou_class
 
-def test(model, test_loader, loss_fn, clear_memory_every):
+def test(model, test_loader, loss_fn, num_clasess, clear_memory_every):
     model.eval()
     test_loss = 0
     correct = 0
     total_images = 0
     total_iou = 0
+    total_iou_cl = np.zeros([1,num_classes])
+    
     with torch.no_grad():
         for batch_idx, (inputs, targets) in enumerate(test_loader):
             inputs, targets = inputs.cuda(), targets.cuda()
@@ -71,14 +77,15 @@ def test(model, test_loader, loss_fn, clear_memory_every):
             test_loss += loss.item()
             _, predicted = outputs.max(1)
             
-            iou = meanIOU(outputs.size()[1], predicted, targets) #sum of meanIOU over classes di tutte le immagini nel batch
-            
-            total_iou += iou.sum().item()  #somma di tytte le singole iou calcolate in precedenza
+            iou, iou_cl = meanIOU(num_classes, predicted, targets) #sum of meanIOU over classes di tutte le immagini nel batch
+            total_iou += iou
+            total_iou_cl += iou_cl
             total_images += len(targets)
             
             # Clear GPU memory periodically
             if clear_memory_every!=0 and batch_idx % clear_memory_every == 0:
                 clear_gpu_memory()
 
-    result= total_iou/total_images
-    return result
+    miou = total_iou/total_images
+    iou_class = total_iou_cl/total_images
+    return miou, iou_class
