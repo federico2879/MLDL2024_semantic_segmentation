da9ef7eeb388eb59bc2fd3882abc5bbdf467bfa8
fabiodanielediena <126665927+fabiodanielediena@users.noreply.github.com>
2024-06-01 16:47:16 +0200

    Update metrics.py
    separate IOU

--- models/metrics.py
+++ models/metrics.py
@@ -51,23 +51,5 @@ def Latency_FPS(model, height, width):
   stdFPS = statistics.stdev(FPS)*1000
   return meanLatency, stdLatency, meanFPS, stdFPS
 
-def fast_hist(pred, target, num_classes):
-    k = (pred >= 0) & (pred < num_classes)
-    return np.bincount(num_classes * pred[k].astype(int) + target[k], minlength = num_classes**2).reshape(num_classes, num_classes)
-
-def per_class_iou(hist):
-    epsilon = 1e-5
-    return (np.diag(hist)+epsilon) / (hist.sum(1) + hist.sum(0) - np.diag(hist) + epsilon)
-
-def meanIOU(num_classes, pred, target):
-  mIOU = 0
-  IOU_classes = np.zeros([1,num_classes])  
-  for i in range(len(pred)):    
-      hist = fast_hist(pred[i].cpu().numpy(), target[i].cpu().numpy(), num_classes)
-      IOU = per_class_iou(hist)
-      IOU_classes = IOU_classes + IOU
-      mIOU = mIOU + sum(IOU)/num_classes 
-  return mIOU, IOU_classes 
-
 def count_params(model):
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
