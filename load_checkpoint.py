3e1b1ba2f63c94e68870cb27e612c0774db6103b
fabiodanielediena <126665927+fabiodanielediena@users.noreply.github.com>
2024-06-17 11:44:37 +0200

    Update load_checkpoint.py

--- load_checkpoint.py
+++ load_checkpoint.py
@@ -1,7 +1,7 @@
 import torch
 import numpy as np
 
-def load_checkpoint(filename="checkpoint.pth.tar"):
+def load_checkpoint(model, optimizer, filename="checkpoint.pth.tar"):
     checkpoint = torch.load(filename)
     model.load_state_dict(checkpoint['state_dict'])
     optimizer.load_state_dict(checkpoint['optimizer'])
@@ -13,5 +13,8 @@ def load_checkpoint(filename="checkpoint.pth.tar"):
     IOU_val = checkpoint['IOU_val']
     loss_val = checkpoint['loss_val']
     return model, optimizer, start_epoch, meanIOU_tr, IOU_tr, loss_tr, meanIOU_val, IOU_val, loss_val
-
-#model, optimizer, start_epoch, meanIOU_tr, IOU_tr, loss_tr, meanIOU_val, IOU_val, loss_val  = load_checkpoint()
+    
+'''
+model, optimizer, start_epoch, meanIOU_tr, IOU_tr, loss_tr, meanIOU_val, IOU_val, loss_val \
+= load_checkpoint(model, optimizer,"/kaggle/working/checkpoint.pth.tar")
+'''
