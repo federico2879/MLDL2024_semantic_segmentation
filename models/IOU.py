a0b565dc6c85a34aa6761e68e21206de264e2780
federico2879 <128393678+federico2879@users.noreply.github.com>
2024-07-08 14:36:28 +0200

    Update IOU.py

--- models/IOU.py
+++ models/IOU.py
@@ -9,12 +9,3 @@ def per_class_iou(hist):
     epsilon = 1e-5
     return (np.diag(hist)) / (hist.sum(1) + hist.sum(0) - np.diag(hist) + epsilon)
 
-def meanIOU(num_classes, pred, target):
-  mIOU = 0
-  IOU_classes = np.zeros([1,num_classes])  
-  for i in range(len(pred)):    
-      hist = fast_hist(pred[i].cpu().numpy(), target[i].cpu().numpy(), num_classes)
-      IOU = per_class_iou(hist)
-      IOU_classes = IOU_classes + IOU
-      mIOU = mIOU + sum(IOU)/num_classes 
-  return mIOU, IOU_classes
